0bb3a5d682a3a42af49084daeb26650b8604d90c
Thomas Kägi <kaegit@gmail.com>
2024-07-07 17:27:01 +0200

    Split ITime up into fine grained interfaces.

--- source/Atmoos.World/ITime.cs
+++ source/Atmoos.World/ITime.cs
@@ -1,13 +1,19 @@
 namespace Atmoos.World;
 
-// ToDo: Split this up into ITimeProvider and ITiming
-public interface ITime
+public interface ITime :
+    ITimeProvider,
+    ITiming
+{ /* The sum of all time operations. */ }
+
+public interface ITimeProvider
 {
     static abstract DateTime Now { get; }
+}
+
+public interface ITiming
+{
     static abstract Tic Tic();
     static abstract TimeSpan Toc(in Tic tic);
-    protected static Tic Start(Func<TimeSpan> mark) => new(mark);
-    protected static TimeSpan Elapsed(in Tic tic) => tic.Elapsed;
 }
 
 public readonly struct Tic
